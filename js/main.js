6301db5efe5f1892b4e4d0dff55a1b9df40f3598
dollyzn <natasjesus1@gmail.com>
2023-03-10 17:11:59 -0300

    fix input placeholder on focus (automatic filling)

--- js/main.js
+++ js/main.js
@@ -1,89 +1,101 @@
-
 (function ($) {
-    "use strict";
+  "use strict";
 
-
-    /*==================================================================
+  /*==================================================================
     [ Focus input ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
-  
-  
-    /*==================================================================
+  $(".input100").each(function () {
+    $(this).on("blur", function () {
+      if ($(this).val().trim() != "") {
+        $(this).addClass("has-val");
+      } else {
+        $(this).removeClass("has-val");
+      }
+    });
+  });
+
+  $(document).ready(function () {
+    setTimeout(function () {
+      if ($("#unit").val().trim() != "") {
+        $("#unit").addClass("has-val");
+      } else {
+        $("#unit").removeClass("has-val");
+      }
+      if ($("#pass").val().trim() != "") {
+        $("#pass").addClass("has-val");
+      } else {
+        $("#pass").removeClass("has-val");
+      }
+    }, 5000);
+  });
+
+  /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+  var input = $(".validate-input .input100");
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+  $(".validate-form").on("submit", function () {
+    var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+    for (var i = 0; i < input.length; i++) {
+      if (validate(input[i]) == false) {
+        showValidate(input[i]);
+        check = false;
+      }
+    }
 
-        return check;
+    return check;
+  });
+
+  $(".validate-form .input100").each(function () {
+    $(this).focus(function () {
+      hideValidate(this);
     });
+  });
 
-
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  function validate(input) {
+    if ($(input).attr("type") == "email" || $(input).attr("name") == "email") {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) == null
+      ) {
+        return false;
+      }
+    } else {
+      if ($(input).val().trim() == "") {
+        return false;
+      }
     }
+  }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    $(thisAlert).addClass("alert-validate");
+  }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    /*==================================================================
+    $(thisAlert).removeClass("alert-validate");
+  }
+
+  /*==================================================================
     [ Show pass ]*/
-    var showPass = 0;
-    $('.btn-show-pass').on('click', function(){
-        if(showPass == 0) {
-            $(this).next('input').attr('type','text');
-            $(this).find('i').removeClass('zmdi-eye');
-            $(this).find('i').addClass('zmdi-eye-off');
-            showPass = 1;
-        }
-        else {
-            $(this).next('input').attr('type','password');
-            $(this).find('i').addClass('zmdi-eye');
-            $(this).find('i').removeClass('zmdi-eye-off');
-            showPass = 0;
-        }
-        
-    });
-
-
+  var showPass = 0;
+  $(".btn-show-pass").on("click", function () {
+    if (showPass == 0) {
+      $(this).next("input").attr("type", "text");
+      $(this).find("i").removeClass("zmdi-eye");
+      $(this).find("i").addClass("zmdi-eye-off");
+      showPass = 1;
+    } else {
+      $(this).next("input").attr("type", "password");
+      $(this).find("i").addClass("zmdi-eye");
+      $(this).find("i").removeClass("zmdi-eye-off");
+      showPass = 0;
+    }
+  });
 })(jQuery);
