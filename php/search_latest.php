a3ed9e1d3e170639d70b7436195d6631ef681335
dollyzn <natasjesus1@gmail.com>
2023-03-29 10:46:39 -0300

    set limit to fetch latest signatures

--- php/search_latest.php
+++ php/search_latest.php
@@ -1,7 +1,7 @@
 <?php
 include("connection.php");
 
-$stmt = $conn->prepare("SELECT unit, xworkId, atendant, date, signature FROM delivered ORDER BY date DESC");
+$stmt = $conn->prepare("SELECT unit, xworkId, atendant, date, signature FROM delivered ORDER BY date DESC LIMIT 20");
 $stmt->execute();
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
